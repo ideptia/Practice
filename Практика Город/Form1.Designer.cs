f57819333d22c642ad1e19470b25876bd33d964c
ideptia <231231234567@mail.ru>
2022-07-04 19:17:38 +0300

    first commit

---
+++ Практика Город/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Практика_Город
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseCaptureChanged += new System.EventHandler(this.pictureBox1_MouseCaptureChanged);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.Move += new System.EventHandler(this.pictureBox1_Move);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton1.Location = new System.Drawing.Point(51, 39);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Дерево";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(51, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Здание";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseMnemonic = false;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(51, 112);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Сплошная полоса";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(51, 145);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(66, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Машина";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(51, 181);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(75, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Светофор";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            this.radioButton5.Enter += new System.EventHandler(this.radioButton5_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Чтобы закончить игру нажмите 2 раза левую кнопку мыши";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Практика Город/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Практика_Город
+{
+    public partial class Form1 : Form
+    {
+        public bool pressA;
+        public bool pressD;
+        public bool pressS;
+        public bool pressW;
+        public int kadr;
+        public Bitmap screen;
+        public Graphics screenG;
+        public Car PlayerCar , tree, roof, leftline,green,red,yellow,whi;
+        double[,] leftlines = new double[1000, 2];
+        public double penalty = 0,vertic=0,horizon=0;
+        public bool flag = false;
+        public int startx, starty;
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+            pressA = false;
+            pressD = false;
+            pressS = false;
+            pressW = false;
+            kadr = 0;
+            screen = new Bitmap(10000, 10000);
+            screenG = Graphics.FromImage(screen);
+           
+            PlayerCar = new Car(250, 400, Pics.textureCar1);
+            tree = new Car(100, 50, Pics.Tree);
+            roof = new Car(100, 90, Pics.Roof);
+            leftline = new Car(175, 70, Pics.LeftLine);
+            green=new Car(190,50,Pics.Green);
+            yellow = new Car(190, 50, Pics.Yellow);
+            red = new Car(190, 50, Pics.Red);
+            whi = new Car(0, 0, Pics.Whi);
+
+            pictureBox1.Image = screen;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {                     
+            this.Text = kadr.ToString();
+            kadr++;
+            pictureBox1.Invalidate();
+            
+            screenG.DrawImage(PlayerCar.pic, PlayerCar.x, PlayerCar.y);
+            screenG.DrawImage(tree.pic, tree.x, tree.y);
+            screenG.DrawImage(roof.pic, roof.x, roof.y);
+            screenG.DrawImage(leftline.pic, leftline.x, leftline.y);
+            screenG.DrawImage(green.pic, green.x, green.y);
+            screenG.DrawImage(yellow.pic, yellow.x, yellow.y);
+            screenG.DrawImage(red.pic, red.x, red.y);
+
+            
+            if (pressA) 
+            {               
+                PlayerCar.x -= 1;
+                for (int i = 0; i <vertic ; i++)
+                {      
+                    for (int j = 0; j < 31; j++)
+                    {
+                        if (leftlines[i, 0] == PlayerCar.x && leftlines[i, 1] == PlayerCar.y+j)
+                        {
+                            penalty++;
+                            MessageBox.Show("Штраф 100 рублей за пересечении сплошной полосы");                           
+                        }
+                    }
+                }
+                if (flag == false && PlayerCar.x == 175 && PlayerCar.y >= 70 && PlayerCar.y <= 100)
+                {
+                    penalty++;
+                    MessageBox.Show("Штраф 100 рублей за пересечении сплошной полосы");
+                }
+            }          
+            if (pressD) PlayerCar.x += 1;
+            if (pressW) PlayerCar.y -= 1;
+            if (pressS) PlayerCar.y += 1;        
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.A) pressA = true;
+            if (e.KeyCode == Keys.D) pressD = true;
+            if (e.KeyCode == Keys.S) pressS = true;
+            if (e.KeyCode == Keys.W) pressW = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A) pressA = false;
+            if (e.KeyCode == Keys.D) pressD = false;
+            if (e.KeyCode == Keys.S) pressS = false;
+            if (e.KeyCode == Keys.W) pressW = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           long  xx = 0, yy = 0;
+            if(radioButton1.Checked)
+            {
+                tree = new Car(Cursor.Position.X, Cursor.Position.Y, Pics.Tree);
+            }
+            if (radioButton2.Checked)
+            {
+                roof = new Car(Cursor.Position.X, Cursor.Position.Y, Pics.Roof);
+            }
+            if (radioButton3.Checked)
+            {
+                
+                leftline = new Car(Cursor.Position.X, Cursor.Position.Y, Pics.LeftLine);
+                leftlines[xx, 0] = Cursor.Position.X;
+                leftlines[xx, 1] = Cursor.Position.Y;
+                vertic++;
+                xx++;
+            }
+            if (radioButton4.Checked)
+            {
+                PlayerCar = new Car(Cursor.Position.X, Cursor.Position.Y, Pics.textureCar1);
+            }
+
+            if (radioButton5.Checked)
+            {
+                red = new Car(Cursor.Position.X, Cursor.Position.Y, Pics.Red);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Dispose();
+            radioButton2.Dispose();
+            radioButton3.Dispose();
+            radioButton4.Dispose();
+            radioButton5.Dispose();
+            button1.Dispose();
+            button2.Dispose();
+            startx=PlayerCar.x;
+            starty = PlayerCar.y;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            screenG.DrawImage(whi.pic, whi.x, whi.y);
+            flag = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton5_Enter(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Общая сумма штрафов составляет" + penalty*100);
+            MessageBox.Show("Начальное местоположение машины x=" + PlayerCar.x + "y=" + PlayerCar.y);
+            MessageBox.Show("Последнее местоположение машины x=" + PlayerCar.x + "y=" + PlayerCar.y);
+        }
+
+        private void pictureBox1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Move(object sender, EventArgs e)
+        {
+            
+        }
+
+        public class Car
+        {
+            public int x;
+            public int y;
+            public Bitmap pic;
+            public Car(int x, int y, Bitmap pic)
+            {
+                this.x = x;
+                this.y = y;
+                this.pic = pic;                  
+            }
+        }
+        public static class Pics
+        {
+            public static Bitmap textureCar1 = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//car1.png");
+            public static Bitmap Tree = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//tree.png");
+            public static Bitmap Roof = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//roof.png");
+            public static Bitmap LeftLine = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//leftline.jpg");
+            public static Bitmap Green= (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//green.png");
+            public static Bitmap Yellow = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//yellow.png");
+            public static Bitmap Red = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//red.png");
+            public static Bitmap Whi = (Bitmap)Image.FromFile(Directory.GetCurrentDirectory() + "//whi.jpg");
+
+        }
+        public static class Rand
+        {
+            public static Random rand = new Random();
+            public static int GetInT(int min, int max)
+            {
+                return rand.Next(min, max);
+            }
+        }
+    }
+}
